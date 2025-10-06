713e4eaac8f58838c408dfae13d98abc95e672c4
emanuelegiacomozzi <emanuelegiacomozzi2018@gmail.com>
2025-10-06 18:00:10 +0200

    modifica css del menu  nell'header

--- frontend/province-comuni-app/src/app/app.ts
+++ frontend/province-comuni-app/src/app/app.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, FormsModule, CommonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('province-comuni-app');
+
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
